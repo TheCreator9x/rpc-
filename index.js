84eb0f31943ffe022e9b584df7377e2af7421b0f
TheCreator9x <159553845+TheCreator9x@users.noreply.github.com>
2024-02-09 08:56:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `/makabelico sonando lleva un billeton`;
+      const newDetails = `/rvs`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
