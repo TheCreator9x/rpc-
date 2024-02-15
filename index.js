7c5592fb9132dad0f95b6d01d2751be073693c0a
TheCreator9x <159553845+TheCreator9x@users.noreply.github.com>
2024-02-15 17:23:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `/just4u`;
+      const newDetails = `/vx`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
